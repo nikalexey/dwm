68d8bbe8f673f0ab93cdcfaf645cddd35082c6f7
nikalexey <nikalexey@gmail.com>
2015-04-05 12:28:01 +0300

    autostart

--- dwm.c
+++ dwm.c
@@ -225,6 +225,7 @@ static void resizemouse(const Arg *arg);
 static void resizerequest(XEvent *e);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutoStartup(void);
 static void scan(void);
 static Bool sendevent(Window w, Atom proto, int m, long d0, long d1, long d2, long d3, long d4);
 static void sendmon(Client *c, Monitor *m);
@@ -1556,6 +1557,12 @@ run(void) {
 }
 
 void
+runAutoStartup(void) {
+	system("cd ~/.dwm; ./autostart_blocking.sh");
+	system("cd ~/.dwm; ./autostart.sh &");
+}
+
+void
 scan(void) {
 	unsigned int i, num;
 	Window d1, d2, *wins = NULL;
@@ -2490,6 +2497,7 @@ main(int argc, char *argv[]) {
 	checkotherwm();
 	setup();
 	scan();
+	runAutoStartup();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
